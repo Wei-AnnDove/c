74b6d74c1ba263eb89e49c5c8955749eaa21aba6
Administrator <Administrator@SK-20231231XLON>
2024-03-11 20:09:10 +0800

    添加项目文件。

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "³ÌÐòÉè¼Æ" << endl;
+	cin >> a >> b;
+	int c;
+	cin >> c;
+	cout << a << endl;
+	cout << a + b << endl;
+	cout << b * c << endl;
+
+	return 0;
+
+
+}
